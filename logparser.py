a117bd1df95178cab50aedf85b15e343a4560689
Hazze <hans.hazelius@gmail.com>
2019-12-17 21:44:03 +0100

    Refactor

--- logparser.py
+++ logparser.py
@@ -3,22 +3,25 @@ import collections
 LOG_REGEX = r"(.*)\s\[(\d+)\]\s(.+)"
 log_entries = {}
 
+def process_logline(line):
+    match = re.search(LOG_REGEX, line)
+    if match:
+        pid = match.group(2)
+        message = match.group(3)
+        full_message= match.group()
+        if pid not in log_entries:
+            log_entries[pid]=collections.deque(maxlen=4)
+        log_entries[pid].append(full_message)
+        if message.startswith("ERROR:"):
+            print(*log_entries[pid], sep='\n')
+            print("----")
+            del(log_entries[pid])
+
 def print_errors(file_name):
     with open(file_name) as logfile:
         for line in logfile:
-            line = line.strip()
-            match = re.search(LOG_REGEX, line)
-            if match:
-                pid = match.group(2)
-                message = match.group(3)
-                full_message= match.group()
-                if pid not in log_entries:
-                    log_entries[pid]=collections.deque(maxlen=4)
-                log_entries[pid].append(full_message)
-                if "ERROR" in message:
-                    print(*log_entries[pid],sep='\n')
-                    print("----")
-                    del(log_entries[pid])
+            process_logline(line.strip())
+
 
 if __name__ == "__main__":
     pass
--- test_logparser.py
+++ test_logparser.py
@@ -7,7 +7,7 @@ import io, sys
 class TestLogparser(unittest.TestCase):
     def test_acceptance_test(self):
         """
-        Acceptance test for log printer
+        Acceptance test for log printer.
         """
         log_input = textwrap.dedent("""\
             2019-4-1 13:32:40 [190] User3 logs in
@@ -35,15 +35,94 @@ class TestLogparser(unittest.TestCase):
             ----
             """)
 
+        self.assertLog(log_input, expected)
+
+    def test_handle_error_in_non_error_message(self):
+        """
+        Output should not be printed if message does not start with 'ERROR:'.
+        """
+        log_input = textwrap.dedent("""\
+            2019-4-1 13:32:40 [190] Some ERROR: occured
+            """)
+
+        expected = ""
+
+        self.assertLog(log_input, expected)
+
+    def test_same_pids(self):
+        """
+        Same pid with multiple errors should produce multiple printouts
+        if they pid are after error line.
+        """
+        log_input = textwrap.dedent("""\
+            2019-4-1 13:33:45 [100] User1 logs in
+            2019-4-1 13:33:55 [100] User1 goes to search page
+            2019-4-1 13:33:56 [100] ERROR: Some exception occured
+            2019-4-2 13:33:45 [100] User2 logs in
+            2019-4-2 13:33:55 [100] User2 goes to search page
+            2019-4-2 13:33:56 [100] ERROR: Some other exception occured
+            """)
+
+        expected = textwrap.dedent("""\
+            2019-4-1 13:33:45 [100] User1 logs in
+            2019-4-1 13:33:55 [100] User1 goes to search page
+            2019-4-1 13:33:56 [100] ERROR: Some exception occured
+            ----
+            2019-4-2 13:33:45 [100] User2 logs in
+            2019-4-2 13:33:55 [100] User2 goes to search page
+            2019-4-2 13:33:56 [100] ERROR: Some other exception occured
+            ----
+            """)
+
+        self.assertLog(log_input, expected)
+
+    def test_max_4_lines(self):
+        """
+        Only the last 4 lines should be included in the printout.
+        """
+        log_input = textwrap.dedent("""\
+            2019-4-1 13:33:45 [100] Error line 1
+            2019-4-1 13:33:45 [200] Nonerror
+            2019-4-1 13:33:55 [100] Error line 2
+            2019-4-2 13:33:55 [200] Nonerror
+            2019-4-1 13:33:55 [100] Error line 3
+            2019-4-1 13:33:55 [100] Error line 4
+            2019-4-1 13:33:55 [100] Error line 5
+            2019-4-2 13:33:56 [100] ERROR: Some other exception occured
+            """)
+
+        expected = textwrap.dedent("""\
+            2019-4-1 13:33:55 [100] Error line 3
+            2019-4-1 13:33:55 [100] Error line 4
+            2019-4-1 13:33:55 [100] Error line 5
+            2019-4-2 13:33:56 [100] ERROR: Some other exception occured
+            ----
+            """)
+
+        self.assertLog(log_input, expected)
+
+    def test_ignore_garbage(self):
+        """
+        Output should not be printed if log line does not match regex.
+        """
+        log_input = textwrap.dedent("""\
+            2019-4-1 13:32:40 No pid
+            """)
+
+        expected = ""
+
+        self.assertLog(log_input, expected)
+
+    def assertLog(self, input_text, expected_text):
         with tempfile.NamedTemporaryFile(mode='w+t') as f:
-            f.writelines(log_input)
+            f.writelines(input_text)
             f.seek(0)
             output = io.StringIO()
             sys.stdout = output
             logparser.print_errors(f.name)
             sys.stdout = sys.__stdout__
-    
-        self.assertMultiLineEqual(expected, output.getvalue())
+        self.assertMultiLineEqual(expected_text, output.getvalue())
+
 
 if __name__ == '__main__':
     unittest.main()
